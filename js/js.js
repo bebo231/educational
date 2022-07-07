51a51e712c5c6b1b79e5bddd7ee6c4d82359437c
bebo231 <108553872+bebo231@users.noreply.github.com>
2022-07-07 17:40:27 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,14 +1,13 @@
-let icon = document.querySelectorAll(".icon-link");
-    for(var i=0 ; i <icon.length ; i++){
-        icon[i].addEventListener("click" , (e)=>{
-            let arrowparent = e.target.parentElement.parentElement;
-            arrowparent.classList.toggle("showMenu");
-        });
-    }
+$('.dropbtn').click(function(){
+    $('.dropdown').toggleClass('display');
+    $('.dropdown2').removeClass('display');
+})
 
 
-let sidebar = document.querySelector(".sidebar");
- let sidebarbtn = document.querySelector(".elso");
- sidebarbtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("close");
-  });
+$('.dropbtn2').click(function(){
+    $('.dropdown2').toggleClass('display');
+})
+
+$('.fa-bars').click(function(){
+    $('.menu-content').toggle();
+})
